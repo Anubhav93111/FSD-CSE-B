d4b909bdc3b2a01e2071358617498ab6d7e96658
Anubhav Kumar <anubhavkumar93111@gmail.com>
2025-09-29 15:21:17 +0530

    Destructing

---
+++ jsDestruct.js
@@ -0,0 +1,15 @@
+const num=[10,20,30,40,50];
+
+const [a,b,c,d,e]=num;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+
+const obj = { name:"abc", id:2, email:"abc@gmail.com"};
+const {name,id,email}=obj;
+console.log(name);
+console.log(id);
+console.log(email);
